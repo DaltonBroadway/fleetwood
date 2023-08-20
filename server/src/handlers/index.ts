bb90ee7cdcd02fd12b6578fc6446331ad99943de
DaltonBroadway <dbroadw3@gmail.com>
2023-08-20 14:56:19 -0400

    move col/row logic to sheet

--- server/src/handlers/index.ts
+++ server/src/handlers/index.ts
@@ -1,16 +1,8 @@
 import { MsgHandlerTuple } from "./types";
 import { handlers as sheet } from "./sheet";
-import { handlers as column } from "./column";
-import { handlers as row } from "./row";
 import { handlers as cell } from "./cell";
 import { handlers as section } from "./section";
 
 export { MsgHandlerTuple } from "./types";
 
-export const handlers: MsgHandlerTuple[] = [
-  ...sheet,
-  ...column,
-  ...row,
-  ...cell,
-  ...section,
-];
+export const handlers: MsgHandlerTuple[] = [...sheet, ...cell, ...section];
