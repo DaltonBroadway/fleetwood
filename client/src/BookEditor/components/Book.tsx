1064711d1dd17e7295e5b3a8327deef1bba41bdd
DaltonBroadway <dbroadw3@gmail.com>
2023-09-08 17:43:25 -0400

    why pass context as a prop

--- client/src/BookEditor/components/Book.tsx
+++ client/src/BookEditor/components/Book.tsx
@@ -1,20 +1,14 @@
-import { useContext, useState } from "react";
-import { SheetContext } from "../contexts/SheetContext";
+import { useState } from "react";
 import { Sheet } from "./Sheet";
 import { Id } from "../../shared/types";
 import { SheetSelector } from "./SheetSelector";
 
 export const Book = () => {
-  const sheetIds = Object.keys(useContext(SheetContext));
   const [activeSheet, setActiveSheet] = useState("vw3g" as Id); // TEMPORARY
   return (
     <div className="book">
       <Sheet id={activeSheet} />
-      <SheetSelector
-        sheetIds={sheetIds}
-        active={activeSheet}
-        setActive={setActiveSheet}
-      />
+      <SheetSelector active={activeSheet} setActive={setActiveSheet} />
     </div>
   );
 };
--- client/src/BookEditor/components/SheetSelector.tsx
+++ client/src/BookEditor/components/SheetSelector.tsx
@@ -2,16 +2,15 @@ import { useContext } from "react";
 import { SheetContext } from "../contexts/SheetContext";
 
 type Props = {
-  sheetIds: string[];
   active: string;
   setActive: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export const SheetSelector = ({ sheetIds, active, setActive }: Props) => {
+export const SheetSelector = ({ active, setActive }: Props) => {
   const sheets = useContext(SheetContext);
   return (
     <div className="sheet-selector">
-      {sheetIds.map((sheetId) => {
+      {Object.keys(useContext(SheetContext)).map((sheetId) => {
         return (
           <div
             className={`sheet-tab${sheetId === active ? " active" : ""}`}
